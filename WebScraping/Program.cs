4d797bdd849bd286e685af3208ba7eab315999fc
Engin <enginaydogdu87@gmail.com>
2021-06-06 12:57:20 +0300

    Add project files.

---
+++ WebScraping/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebScraping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
